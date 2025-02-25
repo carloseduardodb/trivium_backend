fd2073cd37e50e16f55cc5de39e702cc3262c1ff
Carlos Eduardo Dias Batista <carlos.batista@somoseducacao.com.br>
2025-02-24 21:42:19 -0300

    feat: update initialize events

--- internal/domain/usecase/crypto-watch-event.go
+++ internal/domain/usecase/crypto-watch-event.go
@@ -1,20 +1,20 @@
-package event
+package usecase
 
 import (
 	"trivium/internal/domain/repositorier"
 	"trivium/internal/infra/websocket/binance/repository"
 )
 
-type CryptoWatchEvent struct {
+type CryptoWatchEventUseCase struct {
 	Repository repositorier.CryptoStatusRepository
 }
 
-func NewCryptoWatchEvent() *CryptoWatchEvent {
-	return &CryptoWatchEvent{
+func NewCryptoWatchEventUseCase() *CryptoWatchEventUseCase {
+	return &CryptoWatchEventUseCase{
 		Repository: repository.NewCryptoRepository(),
 	}
 }
 
-func (c *CryptoWatchEvent) WatchEvent(cryptos []string) <-chan repositorier.CryptoData {
+func (c *CryptoWatchEventUseCase) WatchEvent(cryptos []string) <-chan repositorier.CryptoData {
 	return c.Repository.StreamCryptoData(cryptos)
 }
